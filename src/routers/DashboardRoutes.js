29147cdbe2e346a28151074541aaa0a6d52f2958
Agustin <re_agustin@hotmail.com>
2022-05-31 00:39:33 -0300

    commit 310522 midnight

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import { Navbar } from "../components/ui/NavBar"
+
+import { DcScreen } from '../components/dc/DcScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
+import { HeroScreen } from "../components/hero/HeroScreen";
+
+
+export const DashboardRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+        <Routes>
+            <Route path="marvel" element={<MarvelScreen/>} />
+            <Route path="dc" element={<DcScreen/>} />
+            <Route path="hero" element={<HeroScreen/>} />
+            
+            <Route path="search" element={<SearchScreen/>} />
+            
+            <Route path="/" element={<MarvelScreen/>} />
+        </Routes>
+    </>
+  )
+}
