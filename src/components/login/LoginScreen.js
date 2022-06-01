b1278c7d6b5e614b122ecc977d13173ef7694c2b
Agustin <re_agustin@hotmail.com>
2022-05-31 22:12:01 -0300

    310522

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const LoginScreen = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true
+    });
+  }
+
+
   return (
-    <div>LoginScreen</div>
+    <div className="container mt5">
+      <h1>LoginScreen</h1>
+      <hr/>
+
+      <button
+        className="btn btn-primary"
+        onClick={handleLogin}
+      > 
+        Login
+      </button>
+    </div>
   )
 }
