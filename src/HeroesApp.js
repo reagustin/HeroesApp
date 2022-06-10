072bd5c8e975640cefc37cab1b5146e136f39c06
Agustin <re_agustin@hotmail.com>
2022-06-09 23:41:55 -0300

    arggg

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { AuthContext } from './auth/authContext';
 import { authReducer } from './auth/authReducer';
 import { AppRouter } from './routers/AppRouter';
@@ -10,6 +10,14 @@ const init = () => {
 export const HeroesApp = () => {
 
   const [ user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    if (!user) return;
+    
+    localStorage.setItem('user', JSON.stringify(user));
+    
+  }, [user])
+  
 
   return (
     <AuthContext.Provider value={{
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
+
 
 export const LoginScreen = () => {
 
   const navigate = useNavigate();
-
+  const {dispatch} = useContext(AuthContext)
+ 
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Juan'        
+      }
+    }
+    
+    dispatch(action);
+    
+    
     navigate('/marvel', {
       replace: true
     });
