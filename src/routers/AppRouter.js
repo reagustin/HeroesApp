8be1ce33ddf34f18381b7aa42677ed8215f04bf4
Agustin <re_agustin@hotmail.com>
2022-06-10 20:57:16 -0300

    ver10062022

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 
@@ -9,9 +11,21 @@ export const AppRouter = () => {
   return (
     <BrowserRouter>
         <Routes>            
-            <Route path="/login" element={<LoginScreen/>} />
+            {/* <Route path="/login" element={<LoginScreen/>} /> */}
+            <Route path="/login" element={
+                <PublicRoute>
+                  <LoginScreen/>
+                </PublicRoute>
+              }
+            />
 
-            <Route path="/*" element={<DashboardRoutes/>} />
+            <Route path="/*" element={
+                <PrivateRoute>
+                  <DashboardRoutes/>
+                </PrivateRoute>
+              }  
+            />
+            {/* <Route path="/*" element={<DashboardRoutes/>} /> */}
 
         </Routes>
     </BrowserRouter>
