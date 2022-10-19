70c334cf98f7107e956b0bf037bb8938b04432f6
Agustin <re_agustin@hotmail.com>
2022-10-18 21:46:54 -0300

    pruebas para tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { render } from "react-dom"; 
 import { HeroesApp } from './HeroesApp';
 
+const divRoot = document.getElementById("root"); 
+render(<HeroesApp />, divRoot);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <HeroesApp />
+// );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <HeroesApp />
-);
-
+// el codigo comentado, es el viejo codigo que corresponde al render del react v18
